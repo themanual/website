83affd0b0b9b0fe0f407d3700ee6fb6c4fd85732
Marc Roberts <marc@neutroncreations.com>
2014-01-27 13:34:17 +0000

    fix cssminify typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Themanual::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = CSSMinify.new
+  config.assets.css_compressor = CSSminify.new
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
