b59ac8f4c94e16e1b6119111b85d6251c7d3b9a0
Paulo Pereira <paulo.zoom@gmail.com>
2014-01-27 18:32:01 +0100

    Redirect root to /issues.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TheManual::Application.routes.draw do
 
-  root 'home#index'
-
+  root to: redirect("/issues")
+  
   get '/issues', to: 'issues#index', as: :issues
   get '/issues/:issue', to: 'issues#show', as: :issue
   get '/issues/:issue/:key/:type', to: 'issues#piece', as: :piece
