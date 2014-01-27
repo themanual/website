f01e558e0c305bbe83c770920db0c129559f0a4c
Paulo Pereira <paulo.zoom@gmail.com>
2014-01-27 18:32:13 +0100

    Redirect blog to subdomain blog.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ TheManual::Application.routes.draw do
   get '/issues/:issue', to: 'issues#show', as: :issue
   get '/issues/:issue/:key/:type', to: 'issues#piece', as: :piece
 
+  get '/blog', to: redirect("http://blog.alwaysreadthemanual.com")
+
 end
