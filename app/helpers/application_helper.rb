7f8607bb5df18469eb064bfcbed1709b8710619a
Paulo Pereira <paulo.zoom@gmail.com>
2014-01-30 14:53:12 +0100

    Fixed bug on page_is_subpath_of helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def page_is_subpath_of path, output = nil
   	if request.path.starts_with? path
-  		output.nil? ? true : output.html_safe!
+  		output.nil? ? true : output.html_safe
   	end
   end
 end
