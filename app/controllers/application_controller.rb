0344ac2c3205ab69ad43e5c2c85342ed5c5b6b35
Marc Roberts <marc@smithers-wired.marc.home.ntrn.net>
2014-02-06 09:31:56 +0000

    take string or array for page title (closes #3)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_order, :has_order?
+  helper_method :current_order, :has_order?, :push_page_title
+  before_filter :prep_page_title
 
   protected
-  	def authenticate_admin_user!
-  		redirect_to new_user_session_path unless current_user.access_level > 0
-  	end
+    def authenticate_admin_user!
+      redirect_to new_user_session_path unless current_user.access_level > 0
+    end
 
-  	def user_signed_in?
+    def user_signed_in?
       !current_user.anon?
     end
 
     def current_user
       @current_user ||= begin
-      	warden.authenticate(:scope => :user) || User.anon_user
+        warden.authenticate(:scope => :user) || User.anon_user
       end
     end
 
@@ -38,4 +39,9 @@ class ApplicationController < ActionController::Base
         @current_order = Shoppe::Order.includes(:order_items => :ordered_item).find_by_id(session[:order_id])
       )
     end
+
+    def push_page_title title
+      ( @page_title << title ).flatten!
+    end
+    def prep_page_title; @page_title ||= []; end
 end
