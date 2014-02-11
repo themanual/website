9016cb202d35403e639117d65dd58bc955eaac9c
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-11 17:48:51 +0100

    Add supporter page.

--- config/routes.rb
+++ config/routes.rb
@@ -19,12 +19,13 @@ TheManual::Application.routes.draw do
 
   root to: redirect("/issues")
 
-  get '/issues', to: 'issues#index', as: :issues
-  get '/issues/:issue', to: 'issues#show', as: :issue
-  get '/issues/:issue/:key/:type', to: 'issues#piece', as: :piece
+  get '/support',                   to: 'home#support',   as: :support
+  get '/issues',                    to: 'issues#index',   as: :issues
+  get '/issues/:issue',             to: 'issues#show',    as: :issue
+  get '/issues/:issue/:key/:type',  to: 'issues#piece',   as: :piece
 
-  post '/buy/:permalink', to: 'orders#update', as: :purchase
-  get '/checkout', to: 'orders#show', as: :basket
+  post  '/buy/:permalink',          to: 'orders#update',  as: :purchase
+  get   '/checkout',                to: 'orders#show',    as: :basket
 
   get '/blog', to: redirect("http://blog.alwaysreadthemanual.com")
 
