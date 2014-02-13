872d7a8900a637654637e6c3466079faae0b3950
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-13 16:55:24 +0100

    Import out-of-the-box slider.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -18,3 +18,15 @@ $("a[href='#']").click(function(event) {
 
 // fitvids
 $(".video").fitVids();
+
+// Support slider
+$(".support-slider").noUiSlider({
+  range: [1, 100],
+  start: 1,
+  step:  1,
+  handles: 1,
+  serialization: {
+    to: $("#support-panel-form-price"),
+    resolution: 1
+  }
+});
