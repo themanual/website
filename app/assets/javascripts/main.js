0e00ca49589e2da0fc2f666c40ddc6453d919740
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-13 17:46:31 +0100

    Remove built-in serialization from slider

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -24,9 +24,5 @@ $(".support-slider").noUiSlider({
   range: [1, 100],
   start: 1,
   step:  1,
-  handles: 1,
-  serialization: {
-    to: $("#support-panel-form-price"),
-    resolution: 1
-  }
+  handles: 1
 });
