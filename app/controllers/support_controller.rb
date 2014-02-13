8805e45f51c81782f6470f01f149d4ee8cd17fee
Marc Roberts <marc@neutroncreations.com>
2014-02-13 17:15:51 +0000

    subscription creation user creation

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -1,2 +1,10 @@
 class SupportController < ApplicationController
+	def create
+
+		u = User.create email: params[:email]
+
+		customer = Stripe::Customer.create(
+	    :card  => params[:stripe_token]
+	  )
+	end
 end
