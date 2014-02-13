e3b4136536a918e406d55a8a57575b933d1b125b
Marc Roberts <marc@neutroncreations.com>
2014-02-13 16:45:54 +0000

    basic stripe form processing in place

--- app/assets/javascripts/modules/subscriptions.js
+++ app/assets/javascripts/modules/subscriptions.js
@@ -1,18 +1,24 @@
 $(function() {
 
-	var handler = function(status, response) {
+	var $form;
 
+	var handler = function(status, response) {
+		if (response.error) {
+			// TODO show something to the user, form is not submitted
+		} else {
+			$form.find('[name=stripe_token]').val(response.id);
+			$form.get(0).submit();
+		}
 	};
 
 	$('form.stripe-form').submit(function(event) {
-    var $form = $(this);
+		event.preventDefault();
+    $form = $(this);
 
     // Disable the submit button to prevent repeated clicks
     $form.find('button').prop('disabled', true);
 
     Stripe.card.createToken($form, handler);
 
-    // Prevent the form from submitting with the default action
-    return false;
   });
 });
