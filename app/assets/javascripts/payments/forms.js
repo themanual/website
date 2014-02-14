3da9c319174a0f75a3b1c4a6e51512a16738da82
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-14 12:03:40 +0100

    Wrap payment form utilities in document ready.

--- app/assets/javascripts/payments/forms.js
+++ app/assets/javascripts/payments/forms.js
@@ -1,3 +1,7 @@
-$('input.cc-number').payment('formatCardNumber');
-$('input.cc-exp').payment('formatCardExpiry');
-$('input.cc-csc').payment('formatCardCVC');
+$(function() {
+
+  $('input.cc-number').payment('formatCardNumber');
+  $('input.cc-exp').payment('formatCardExpiry');
+  $('input.cc-csc').payment('formatCardCVC');
+  
+});
