6135cb85a5588b6e49e0f06c7a8f03aeb3e752db
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-14 13:26:10 +0100

    Break credit card plugins into own files.

--- app/assets/javascripts/payments/plugins/validatecreditcard.jquery.js
+++ app/assets/javascripts/payments/plugins/validatecreditcard.jquery.js
@@ -34,26 +34,5 @@
     return valid;
 
   }
-
-  $.fn.parseCreditCard = function() {
-    // Stored for optimization
-    var $this = $(this);
-
-    // Form fields
-    var $number = $this.find('.cc-number'),
-        $cvc    = $this.find('.cc-csc'),
-        $exp    = $this.find('.cc-exp');
-
-    var exp = $.payment.cardExpiryVal($exp.val());
-
-    var stripe_data = {
-      number:     $number.val().replace(/\s+/g, ''),
-      cvc:        $cvc.val().replace(/\s+/g, ''),
-      exp_month:  exp.month,
-      exp_year:   exp.year
-    };
-
-    return stripe_data;
-  }
-
+  
 }(jQuery));
