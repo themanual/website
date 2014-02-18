2e997efc734aeeb5636e36d0b70e03877d2531bb
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-18 14:48:19 +0100

    Remove credit card plugin.

--- app/assets/javascripts/payments/subscriptions.js
+++ app/assets/javascripts/payments/subscriptions.js
@@ -6,29 +6,23 @@ $(function() {
     var $form = $(this);
 
     // Response handler
-    var stripeHandler = function(status, response) {
+    var stripeResponseHandler = function(status, response) {
+
+      var $this = $(this); // 'this' is the form, as set by _.bind
+
       if (response.error) {
         // TODO show something to the user, form is not submitted
       } else {
-        $form.find('[name=stripe_token]').val(response.id);
-        $form.get(0).submit();
+        $this.find('[name=stripe_token]').val(response.id);
+        $this.get(0).submit();
       }
     };
 
-    $form.validate();
-
-    $form.submit(function(event) {
-
-      var $this           = $(this);
-
+    // Important! Validation plugin must come before submit.
+    $form.validate().submit(function(event) {
       event.preventDefault();
       event.stopPropagation()
-
-      if ($this.validateCreditCard()) {
-        // TODO Change button name
-        Stripe.card.createToken($this.parseCreditCard(), stripeHandler);
-      }
-
+      Stripe.card.createToken($(this).parseCreditCard(), _.bind(stripeResponseHandler, this));
       return false;
     });
 
