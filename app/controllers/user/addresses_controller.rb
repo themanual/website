dc659c9ef177b53ba3cd4f8ac24760ff5d55e7fd
Marc Roberts <marc@neutroncreations.com>
2014-02-20 17:04:02 +0000

    set users default shipping address when saving first address

--- app/controllers/user/addresses_controller.rb
+++ app/controllers/user/addresses_controller.rb
@@ -3,6 +3,10 @@ class User::AddressesController < ApplicationController
 	def create
 		@address = current_user.addresses.new address_params
 		if (@address.save)
+
+			# save as users shipping address if they don't have one (new user)
+			current_user.update_attribute(:shipping_address_id, @address.id) if current_user.shipping_address_id.nil?
+
 			redirect_to account_path
 		else
 			if params[:from_support]
