fb3980aba86024de4d5898a08dccd67a17103006
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-24 13:38:23 +0100

    Take speed parameter on animatecss

--- app/assets/javascripts/base/plugins/animate.jquery.js
+++ app/assets/javascripts/base/plugins/animate.jquery.js
@@ -1,6 +1,8 @@
 (function($) {
-  $.fn.animatecss = function(type) {
+  $.fn.animatecss = function(type, speed) {
     return this.each(function() {
+      var klass = "animated " + type;
+      if (speed) { klass += " " + speed; }
       $(this)
         .addClass("animated "+type)
         .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
