9c9560819dfc47d780f472c4b2be85f843f16601
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-24 16:19:04 +0100

    Simplify $.fn.animatecss code, accept callback.

--- app/assets/javascripts/base/plugins/animate.jquery.js
+++ app/assets/javascripts/base/plugins/animate.jquery.js
@@ -1,12 +1,12 @@
 (function($) {
-  $.fn.animatecss = function(type, speed) {
+  $.fn.animatecss = function(type, callback) {
     return this.each(function() {
       var klass = "animated " + type;
-      if (speed) { klass += " " + speed; }
       $(this)
         .addClass("animated "+type)
         .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
           $(this).removeClass(type);
+          if (_.isFunction(callback)) { callback(); }
         });
     });
   }
