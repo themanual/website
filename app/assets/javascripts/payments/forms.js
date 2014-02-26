b0ad4b6dfda7fc773e8a4fa6bbb7cdb3b03aa8e6
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-26 12:59:51 +0100

    Add more selectors to payment form fields autocompletion

--- app/assets/javascripts/payments/forms.js
+++ app/assets/javascripts/payments/forms.js
@@ -1,7 +1,22 @@
 $(function() {
 
-  $('input.cc-number').payment('formatCardNumber');
-  $('input.cc-exp').payment('formatCardExpiry');
-  $('input.cc-csc').payment('formatCardCVC');
-  
+  var CreditCardSelectors = {
+    num: [
+      'input.cc-number',
+      '[data-stripe=number]',
+      '[autocompletetype=cc-number]']
+    exp: [
+      'input.cc-exp',
+      '[autocompletetype=cc-exp]']
+    csc: [
+      'input.cc-csc',
+      'input.cc-cvc',
+      '[data-stripe=cvc]',
+      '[autocompletetype=cc-csc]']
+  };
+
+  $(CreditCardSelectors.num.join(",")).payment('formatCardNumber');
+  $(CreditCardSelectors.exp.join(",")).payment('formatCardExpiry');
+  $(CreditCardSelectors.csc.join(",")).payment('formatCardCVC');
+
 });
