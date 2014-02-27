f0d4b65ea615fac7501bb5e6d6ae28a637086db2
Paulo Pereira <paulo.zoom@gmail.com>
2014-02-27 19:21:58 +0100

    change quotes

--- app/assets/javascripts/base/misc.js
+++ app/assets/javascripts/base/misc.js
@@ -1,17 +1,17 @@
 $(function() {
 
-  $(".header-nav-menu > li.has-dropdown > a").click(function() {
-    $(this).parent().toggleClass("is-expanded");
+  $('.header-nav-menu > li.has-dropdown > a').click(function() {
+    $(this).parent().toggleClass('is-expanded');
     return false;
   });
 
   // Don't follow # hrefs
-  $("a[href='#']").click(function(event) {
+  $('a[href=#]').click(function(event) {
     return false;
   });
 
   // fitvids
-  $(".video").fitVids();
+  $('.video').fitVids();
 
   $('select.chosen').chosen();
 
