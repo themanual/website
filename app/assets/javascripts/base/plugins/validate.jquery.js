e4039313165b178b93605035b42e71762c5df154
Paulo Pereira <paulo.zoom@gmail.com>
2014-03-05 18:40:15 +0100

    More relaxed email validation

--- app/assets/javascripts/base/plugins/validate.jquery.js
+++ app/assets/javascripts/base/plugins/validate.jquery.js
@@ -7,7 +7,7 @@
     SPECIAL_TYPES:    { check: 'checkbox', radio: 'radio' },
     HTML_INPUT_TYPES: ['email', 'number'],
     PATTERNS:  {
-      email:    /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i,
+      email:    /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.([a-z]{2,})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i,
       number:   /^\d+([,.]\d+)?$/,
       required: /\S+/
     }
