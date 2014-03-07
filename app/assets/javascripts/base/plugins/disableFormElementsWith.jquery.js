833bac050b704439ad39a11340cb2797f9ec3b56
Paulo Pereira <paulo.zoom@gmail.com>
2014-03-07 13:12:43 +0100

    Undo overrides to bower version of jquery-ujs, move them locally.

--- app/assets/javascripts/base/plugins/disableFormElementsWith.jquery.js
+++ app/assets/javascripts/base/plugins/disableFormElementsWith.jquery.js
@@ -1,5 +1,8 @@
 (function($) {
 
+  // TODO remove when https://github.com/rails/jquery-ujs/pull/365 is merged
+  $.rails.disableSelector = 'input[data-disable-with]:enabled, button[data-disable-with]:enabled, textarea[data-disable-with]:enabled';
+
   $.fn.disableFormElementsWith = function(text) {
     return this.each(function() {
       var $form = $(this).is('form') ? $(this) : $(this).closest('form');
