ef11725b51a077fa875f49ec90e589f9c8555eeb
Marc Roberts <marc@neutroncreations.com>
2014-04-30 15:25:14 +0100

    make user object available in login email

--- app/mailers/sessions_mailer.rb
+++ app/mailers/sessions_mailer.rb
@@ -1,7 +1,8 @@
 class SessionsMailer < TheManual::Mailer
 
-	def login_token token
-		@token = token
+	def login_token user, token
+    @token = token
+    @user = user
 
 		mail to: token.email_address.email, subject: 'Login to The Manual'
 	end
