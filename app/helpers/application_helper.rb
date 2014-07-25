e5fff13edd7ff22f7c1a9efa65b57cb505ab94d0
Paulo Pereira <paulo.zoom@gmail.com>
2014-07-25 17:30:04 +0200

    whitespace fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def page_is_subpath_of path, output = nil
-  	if request.path.starts_with? path
-  		output.nil? ? true : output.html_safe
-  	end
+    if request.path.starts_with? path
+      output.nil? ? true : output.html_safe
+    end
   end
 
   def page_is path, output = nil
@@ -16,11 +16,11 @@ module ApplicationHelper
   end
 
   def include_payment_assets
-  	content_for :footer do
-  		javascript_include_tag("https://js.stripe.com/v2/") +
+    content_for :footer do
+      javascript_include_tag("https://js.stripe.com/v2/") +
       javascript_include_tag(:payments) +
-  		javascript_tag("Stripe.setPublishableKey('#{Rails.configuration.stripe[:publishable_key]}');")
-  	end
+      javascript_tag("Stripe.setPublishableKey('#{Rails.configuration.stripe[:publishable_key]}');")
+    end
   end
 
   def link_to_current(name, options, html_options = {})
