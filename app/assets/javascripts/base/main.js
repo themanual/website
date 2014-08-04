b7224067c263886634381b92f280872b4ed8d06e
Paulo Pereira <paulo.zoom@gmail.com>
2014-08-04 11:51:27 +0200

    Remove old JS

--- app/assets/javascripts/base/main.js
+++ app/assets/javascripts/base/main.js
@@ -1,22 +1,5 @@
 $(function() {
 
-  $(document).on('keypress', ':not(:input)', function(event) {
-    if (!$(event.target).is(':input') && event.which == 100) {
-      event.stopPropagation();
-      $("html").toggleClass('debug');
-    }
-  });
-
-  $('.header-nav-menu > li.has-dropdown > a').click(function() {
-    $(this).parent().toggleClass('is-expanded');
-    return false;
-  });
-
-  // Don't follow # hrefs
-  $('a[href=#]').click(function(event) {
-    return false;
-  });
-
   // fitvids
   $('.video').fitVids();
 
