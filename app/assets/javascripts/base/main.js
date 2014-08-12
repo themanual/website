8b69a59e1df4a408dee8c4dd33520102470b0253
Paulo Pereira <paulo.zoom@gmail.com>
2014-08-12 16:52:47 +0200

    Add responsive sidebar

--- app/assets/javascripts/base/main.js
+++ app/assets/javascripts/base/main.js
@@ -6,6 +6,30 @@ $(function() {
   $('form').validate();
   // Enable popovers
   $(document).popovers();
+
+  $(".toggle-sidebar").click(function () {
+    var SIDEBAR_SPEED = 250;
+    var $sidebar = $('.sidebar');
+    var $button  = $(this);
+
+    if ($sidebar.is(':visible')) {
+      $sidebar
+        .stop(true)
+        .fadeOut({duration: SIDEBAR_SPEED})
+        .slideUp({duration: SIDEBAR_SPEED, queue: false });
+      $button.removeClass('expanded');
+    }
+    else {
+      $sidebar
+        .stop(true)
+        .fadeIn({duration: SIDEBAR_SPEED})
+        .hide()
+        .slideDown({duration: SIDEBAR_SPEED, queue: false});
+      $button.addClass('expanded');
+    }
+
+  });
+
   // External links
   $(document).on('click', 'a[rel="external"]', function() {
     window.open($(this).attr('href'));
