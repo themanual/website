416ec1de0828df87a5e937766de13b1376aef3f2
Paulo Pereira <paulo.zoom@gmail.com>
2014-08-12 18:53:31 +0200

    add js comment
    Conflicts:
    	app/assets/javascripts/base/main.js

--- app/assets/javascripts/base/main.js
+++ app/assets/javascripts/base/main.js
@@ -1,9 +1,38 @@
+// Enable fastclick
+FastClick.attach(document.body);
+// Enable viewport-units-buggyfill
+window.viewportUnitsBuggyfill.init();
+
 $(function() {
 
   // Enable form validation
   $('form').validate();
   // Enable popovers
   $(document).popovers();
+
+  // Toggle Sidebar
+  $(".toggle-sidebar").click(function () {
+    var SIDEBAR_SPEED = 250;
+    var $sidebar = $('.sidebar');
+    var $button  = $(this);
+
+    if ($sidebar.is(':visible')) {
+      $sidebar
+        .stop(true)
+        .fadeOut({duration: SIDEBAR_SPEED})
+        .slideUp({duration: SIDEBAR_SPEED, queue: false });
+      $button.removeClass('expanded');
+    }
+    else {
+      $sidebar
+        .stop(true)
+        .fadeIn({duration: SIDEBAR_SPEED})
+        .hide()
+        .slideDown({duration: SIDEBAR_SPEED, queue: false});
+      $button.addClass('expanded');
+    }
+  });
+
   // External links
   $(document).on('click', 'a[rel="external"]', function() {
     window.open($(this).attr('href'));
