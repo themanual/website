1adf0973f50ba3d44656bdc58ce494e34316cb71
Paulo Pereira <paulo.zoom@gmail.com>
2014-08-25 18:47:21 +0200

    Change illo_basepath to model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,8 @@ class Article < Piece
 	def lesson
 		@lesson ||= self.issue.lessons.where(author_id: self.author_id).first
 	end
+
+  def illo_basepath
+    "illustrations/editorial/issues/#{self.issue_number}/#{self.author_slug}"
+  end
 end
