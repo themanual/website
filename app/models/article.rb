590a5987340d4f583faa73bc5e1d2eb3b6d721f7
Paulo Pereira <paulo.zoom@gmail.com>
2014-08-26 11:40:13 +0200

    tabs to spaces

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < Piece
 
-	def lesson
-		@lesson ||= self.issue.lessons.where(author_id: self.author_id).first
-	end
+  def lesson
+    @lesson ||= self.issue.lessons.where(author_id: self.author_id).first
+  end
 
   def illo_basepath
     "illustrations/editorial/issue-#{self.issue_number}/#{self.illustrator.parameterize}"
