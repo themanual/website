fa8f7d0092edb1da642b1eb021b668e3107efb56
Paulo Pereira <paulo.zoom@gmail.com>
2014-09-03 18:03:57 +0200

    KS: redirect /store to shopify

--- config/routes.rb
+++ config/routes.rb
@@ -36,10 +36,11 @@ TheManual::Application.routes.draw do
   get '/blog', to: redirect('http://blog.themanual.org')
 
   # STORE
-  get '/store',              to: 'store#index',        as: :shop
-  get '/store/issues',       to: 'store#issues',       as: :shop_issues
-  get '/store/subscription', to: 'store#subscription', as: :shop_subscription
-  get '/store/issue/:issue', to: 'store#issue',        as: :shop_issue
+  # get '/store',              to: 'store#index',        as: :shop
+  # get '/store/issues',       to: 'store#issues',       as: :shop_issues
+  # get '/store/subscription', to: 'store#subscription', as: :shop_subscription
+  # get '/store/issue/:issue', to: 'store#issue',        as: :shop_issue
+  get '/store', to: redirect('http://store.themanual.org'), as: :shop
 
   # ABOUT
   get '/about', to: 'about#index',  as: :about
