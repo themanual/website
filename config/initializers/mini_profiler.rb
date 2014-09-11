6802daf691d1e40480540976b1de1fb1a7ba7a3a
Marc Roberts <marc@neutroncreations.com>
2014-09-11 20:30:05 +0100

    reposition miniprofile middleware

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,2 +1,4 @@
+Rails.application.middleware.delete(Rack::MiniProfiler)
+Rails.application.middleware.insert_after(ActionDispatch::Static, Rack::MiniProfiler)
 
 Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemcacheStore
