ec44b24f0fd8a4674e68c8e335e821702eb4ffee
Paulo Pereira <paulo.zoom@gmail.com>
2014-09-24 18:31:07 +0200

    Fixes to issue 4 page

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -8,14 +8,18 @@ class IssuesController < ApplicationController
 
   def show
     issue_no = params[:issue].to_i
-    render :soon and return if issue_no == 4
+    if issue_no == 4
+      metadata "og:title",        "Issue 4"
+      metadata "description",     "Issue of The Manual is coming soon. Featuring Jeniffer Brook, David Cole, Paul Ford, Diana Kimball, Wilson Miner, and Craig Mod."
+      render :soon
+    else
+      # TODO: Ensure only visible issues can be seen (public, or purchased by current user)
+      @issue = Issue.published.public.where(number: issue_no).first
+      redirect_to read_path and return unless @issue
 
-    # TODO: Ensure only visible issues can be seen (public, or purchased by current user)
-    @issue = Issue.published.public.where(number: issue_no).first
-    redirect_to read_path and return unless @issue
-
-    metadata "og:title",        "Issue #{@issue.number}"
-    metadata "description",     "Issue #{@issue.number} of The Manual, with #{@issue.authors.map(&:name).to_sentence}."
+      metadata "og:title",        "Issue #{@issue.number}"
+      metadata "description",     "Issue #{@issue.number} of The Manual, with #{@issue.authors.map(&:name).to_sentence}."
+    end
   end
 
 end
