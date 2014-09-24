f24584f273db4877cf7b363ccf09b58321ded009
Paulo Pereira <paulo.zoom@gmail.com>
2014-09-24 17:36:23 +0200

    Add "soon" page for Issue 4

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -7,15 +7,15 @@ class IssuesController < ApplicationController
   end
 
   def show
+    issue_no = params[:issue].to_i
+    render :soon and return if issue_no == 4
+
     # TODO: Ensure only visible issues can be seen (public, or purchased by current user)
-    @issue = Issue.where(number: params[:issue].to_i).first
+    @issue = Issue.published.public.where(number: issue_no).first
+    redirect_to read_path and return unless @issue
 
-    if @issue && @issue.published?
-      metadata "og:title",        "Issue #{@issue.number}"
-      metadata "description",     "Issue #{@issue.number} of The Manual, with #{@issue.authors.map(&:name).to_sentence}."
-    end
-
-    redirect_to read_path unless @issue && @issue.published?
+    metadata "og:title",        "Issue #{@issue.number}"
+    metadata "description",     "Issue #{@issue.number} of The Manual, with #{@issue.authors.map(&:name).to_sentence}."
   end
 
 end
