1d0d7184828befebf18229604259deca8d2b09f4
Paulo Pereira <paulo.zoom@gmail.com>
2014-10-24 18:49:28 +0200

    Access control on topics page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,12 @@
 class TopicsController < ApplicationController
   def show
-    @pieces = Piece.tagged_with(params[:topic])
+    @pieces = Piece.tagged_with(params[:topic]).joins(:issue).where(issue: current_user.visible_issues).order('issues.number DESC, pieces.position ASC')
     @breadcrumb = true
     
     title    "Topics"
     title    params[:topic].titlecase
     metadata "og:title", "Topic: #{params[:topic].titlecase}"
-    metadata "description", "The Manual’s #{@pieces.count} #{'piece'.pluralize(@pieces.count)} on #{params[:topic].titlecase}."
+    metadata "description", "The Manual’s #{@pieces.size} #{'piece'.pluralize(@pieces.size)} on #{params[:topic].titlecase}."
 
     redirect_to read_path if @pieces.blank?
   end
