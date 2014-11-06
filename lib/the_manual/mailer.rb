7f5940bca29a4573e8d545ded0c3eef651dacf17
Paulo Pereira <paulo.zoom@gmail.com>
2014-11-06 18:54:01 +0000

    clear up base mailer

--- lib/the_manual/mailer.rb
+++ lib/the_manual/mailer.rb
@@ -1,24 +1,17 @@
 class TheManual::Mailer < ActionMailer::Base
 
-	default :from => "The Manual <hi@themanual.org>"
+  default :from => "The Manual <hi@themanual.org>"
   layout 'email'
 
   class Interceptor
     def self.delivering_email(mail)
-
       mail.subject = "[#{Rails.env}] #{mail.subject}" if Rails.env.development?
-
       mail.headers({
-        #  disable click tracking for now
-		  	'X-MC-Track' => 'opens',
-		  	'X-MC-Autotext' => 'true',
-		  	'X-MC-GoogleAnalytics' => 'www.alwaysreadthemanual.com,alwaysreadthemanual.com',
-		  	'X-MC-GoogleAnalyticsCampaign' => 'system_email',
-		  	'X-MC-Metadata' => {
-			  		'environment' => Rails.env
-				}.to_json,
-				'X-MC-URLStripQS' => 'true'
-		  })
+        'X-MC-Track' => 'opens',
+        'X-MC-Autotext' => 'true',
+        'X-MC-Metadata' => {'environment' => Rails.env}.to_json,
+        'X-MC-URLStripQS' => 'true'
+      })
     end
   end
 
