9b35c1298f3036d2f43058a1acc15ef80844bdd6
Paulo Pereira <paulo.zoom@gmail.com>
2014-11-11 17:10:59 +0000

    Email us on MODE=TEST

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,8 +1,9 @@
 class SubscriptionMailer < TheManual::Mailer
 
   def shipping_address_update to, data
-    @data = options[:data]
-    mail to: options[:to], subject: "Issue #{@data[:issue_number]} is ready to ship: Update your address!"
+    @data = data
+    to = "hi@themanual.org" if ENV['MODE'] == "TEST"
+    mail to: to, subject: "Issue #{@data[:issue_number]} is ready to ship: Update your address!"
   end
 
 end
