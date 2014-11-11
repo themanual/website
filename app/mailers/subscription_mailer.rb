2097358fc4207862f4bb639febec4d8871486f4b
Paulo Pereira <paulo.zoom@gmail.com>
2014-11-11 18:27:19 +0000

    Change shipping address update email subject

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -3,7 +3,7 @@ class SubscriptionMailer < TheManual::Mailer
   def shipping_address_update to, data
     @data = data
     to = "hi@themanual.org" if ENV['MODE'] == "TEST"
-    mail to: to, subject: "Update your subscription address for issue #{@data[:issue_number]}"
+    mail to: to, subject: "Update your subscription address for Issue #{@data[:issue_number]}"
   end
 
 end
