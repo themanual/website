071466b52a260a9a2af7ec8d10cc8c82e912ea7b
Marc Roberts <marc@neutroncreations.com>
2014-11-19 09:40:35 +0000

    sort out downloads

--- app/controllers/user/account_controller.rb
+++ app/controllers/user/account_controller.rb
@@ -1,6 +1,23 @@
 class User::AccountController < ApplicationController
   before_filter :authenticate_user!
   def show
-    @downloads = current_user.issues_with_downloads
+
+    @visible_downloads = {}
+
+    issues_with_downloads = current_user.issues_with_downloads
+
+    issues_with_downloads.each do |issue|
+      issue_downloads = []
+
+      issue.downloads.each do |dl|
+        if issue.ownerships.where(user_id: current_user.id).any? {|o| o.can_access? dl.medium.downcase }
+          issue_downloads << dl
+        end
+      end
+
+      if issue_downloads.size > 0
+        @visible_downloads[issue.number] = issue_downloads
+      end
+    end
   end
 end
