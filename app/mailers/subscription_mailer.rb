f8a80818381a6d54c7a1eea0ffd6d637f28470cb
Paulo Pereira <paulo.zoom@gmail.com>
2014-11-20 17:56:42 +0100

    use helpers in subscriber emails paths

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -9,13 +9,13 @@ class SubscriptionMailer < TheManual::Mailer
   def new_issue_digital_editions to, data
     @data = data
     to = "hi@themanual.org" if ENV['MODE'] == "TEST"
-    mail to: to, subject: "Your digital editions of Issue #{@data[:issue_number]} are now available"
+    mail to: to, subject: "Your digital editions of Issue #{@data[:issue_number]} are now available!"
   end
 
   def new_issue_web_edition to, data
     @data = data
     to = "hi@themanual.org" if ENV['MODE'] == "TEST"
-    mail to: to, subject: "Your web edition of Issue #{@data[:issue_number]} is now available"
+    mail to: to, subject: "Your web edition of Issue #{@data[:issue_number]} is now available!"
   end
 
 end
