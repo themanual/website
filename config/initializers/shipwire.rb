2047a9480c273ffcba37a866ebf3fbb74ce04285
Marc Roberts <marc@neutroncreations.com>
2014-11-21 10:42:42 +0000

    extend shipwire timeout, dynamic environment

--- config/initializers/shipwire.rb
+++ config/initializers/shipwire.rb
@@ -2,7 +2,7 @@ Shipwire.config do |config|
   config.username = ENV['SHIPWIRE_USERNAME']
   config.password = ENV['SHIPWIRE_PASSWORD']
 
-  config.environment = :live
+  config.environment = Rails.env.production? ? :live : :sandbox
 
-  config.timeout = 3
+  config.timeout = 30
 end
