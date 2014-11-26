1ec843f856ae07e58aa3586f53501f570e2e263e
Marc Roberts <marc@neutroncreations.com>
2014-11-26 14:23:56 +0000

    repoint airbrake notifier

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,6 @@
 Airbrake.configure do |config|
-  config.host = 'exceptions.codebasehq.com'
-  config.api_key = 'a5824f8f-a80f-e069-0d46-5c3a9741d041'
+  config.api_key = '47e58d01758f7e0528b5baf96751a9d1'
+  config.host    = 'airbrake.neutroncreations.com'
+  config.port    = 80
+  config.secure  = config.port == 443
 end
