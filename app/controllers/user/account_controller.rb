83dbfaa372bfd8099d729161b3962fabc09cb07d
Marc Roberts <marc@neutroncreations.com>
2015-02-19 15:09:55 +0000

    enable email updates from users

--- app/controllers/user/account_controller.rb
+++ app/controllers/user/account_controller.rb
@@ -34,6 +34,6 @@ class User::AccountController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name)
+      params.require(:user).permit(:first_name, :last_name, :email)
     end
 end
