4d043b72c13da2fa04e631c9c56c2a17f0687485
Paulo <hi@paulo.is>
2015-03-13 15:18:54 +0100

    Bunch of updates to nav

--- app/assets/javascripts/base/main.js
+++ app/assets/javascripts/base/main.js
@@ -12,9 +12,9 @@ $(function() {
   $('select[data-shipping-address=country]').fillCountryFromGeo();
 
   // Toggle Sidebar
-  $(".toggle-sidebar").bind('click', function() {
+  $(".js-toggle-sidebar").bind('click', function() {
     var SIDEBAR_SPEED = 250;
-    var $sidebar = $('.sidebar');
+    var $sidebar = $('.js-sidebar');
     var $button  = $(this);
 
     if ($sidebar.is(':visible')) {
@@ -22,7 +22,7 @@ $(function() {
         .stop(true)
         .fadeOut({duration: SIDEBAR_SPEED})
         .slideUp({duration: SIDEBAR_SPEED, queue: false });
-      $button.removeClass('expanded');
+      $button.removeClass('is-expanded');
     }
     else {
       $sidebar
@@ -30,14 +30,14 @@ $(function() {
         .fadeIn({duration: SIDEBAR_SPEED})
         .hide()
         .slideDown({duration: SIDEBAR_SPEED, queue: false});
-      $button.addClass('expanded');
+      $button.addClass('is-expanded');
     }
     if (typeof analytics !== 'undefined') { analytics.track('Toggled Sidebar'); }
   });
 
   $(".masthead__statement__close").click(function() {
     $(this).closest(".masthead__statement").slideUp('fast');
-    $.cookie('hide_statement', '1', { path: '/', expires: 365 });
+    //$.cookie('hide_statement', '1', { path: '/', expires: 365 });
   });
 
   // External links
